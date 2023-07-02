152cf0371adb36d86bd3e7adfbafe59e997e2920
Kristian Kosorin <kristian@kosorin.eu>
2023-07-02 11:18:53 +0200

    Updated homepage - WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-"use client";
-
-import AddTransactionForm from "@/components/forms/addTransactionForm";
+import AddTransaction from "@/components/addTransaction";
+import AddTransactionDialog from "@/components/addTransactionDialog";
+import Transactions from "@/components/transactions";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AddTransactionForm  />
+    <main className="flex min-h-screen flex-col items-center justify-between gap-2 p-24">
+      <Transactions />
+      <AddTransactionDialog>
+        <AddTransaction />
+      </AddTransactionDialog>
     </main>
   );
 }
