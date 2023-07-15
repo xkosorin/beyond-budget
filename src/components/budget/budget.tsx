5177896e26dfc390853ac27da8afae9e02bcd91e
Kristián Kosorín <kristian@kosorin.eu>
2023-07-15 11:05:34 +0200

    Fixed package.json

--- src/components/budget/budget.tsx
+++ src/components/budget/budget.tsx
@@ -10,13 +10,14 @@ type Props = {
 
 const Budget = ({ budget }: Props) => {
   const value = Math.floor(((budget.size - budget.spent) / budget.size) * 100);
+  const remaining = (budget.size - budget.spent).toFixed(2);
 
   return (
     <div className="flex w-full min-w-[80px] flex-col items-start justify-between whitespace-nowrap py-2 text-sm font-bold md:text-base xl:min-w-[120px]">
       <div className="flex w-full flex-row justify-between">
         <div className="flex-shrink-0 gap-2 truncate">{budget.title}</div>
         <span className="self-end">
-          {budget.size - budget.spent} / {budget.size} €
+          {remaining} / {budget.size} €
         </span>
       </div>
       <Progress
@@ -24,7 +25,7 @@ const Budget = ({ budget }: Props) => {
         progressColor={cn(
           value > 60 && "bg-[#3FCF8E]",
           value <= 60 && value > 20 && "bg-[#F6AE2D]",
-          value <= 20 && "bg-[#FF6369]"
+          value <= 20 && "bg-[#FF6369]",
         )}
       />
     </div>
