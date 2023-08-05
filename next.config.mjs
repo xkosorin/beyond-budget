c938566428501bed0436ee13d38ce104d6db6ca2
Kristian Kosorin <kristian@kosorin.eu>
2023-08-05 09:53:16 +0200

    Updated dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withPWAInit from "@imbios/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
